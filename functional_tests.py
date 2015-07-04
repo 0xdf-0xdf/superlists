1d0d4c5b1f62de94fd7c617a6788c8033d1b4726
David <farbish2@gmail.com>
2015-07-04 02:54:23 +0000

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,12 +18,12 @@ class NewVisitorTest(unittest.TestCase):
 
     # She notices the page title and header mention to-do lists
     self.assertIn('To-Do', self.browser.title)
-    header_text = self.browser.find_element_by_tag_name('h1').test
+    header_text = self.browser.find_element_by_tag_name('h1').text
     self.assertIn('To-Do', header_text)
 
     # She is invited to enter a to-do item straight away
     inputbox = self.browser.find_element_by_id('id_new_item')
-    self.assetEqual(
+    self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
     )
@@ -34,12 +34,13 @@ class NewVisitorTest(unittest.TestCase):
 
     # When she hits enter, the page updates, and now the page lists
     # "1: Buy peacock feathers" as an item in a to-do list
-    inputbox.send_keys('Keys.ENTER')
+    inputbox.send_keys(Keys.ENTER)
 
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
     self.assertTrue(
-      any(row.text == '1: Buy peacock feathers' for row in rows)
+      any(row.text == '1: Buy peacock feathers' for row in rows),
+      "New to-do item did not appear in table"
     )
 
     # There is still a text box inviting her to add another item. She
@@ -50,7 +51,7 @@ class NewVisitorTest(unittest.TestCase):
 
 
     # The page updates again, and now shows both items on her list
- 
+
     # Edith wonders wheather the site will remember her list. Then she sees
     # that the site has generated a unique URL for her -- there is some
     # explanatory text to that effect.
